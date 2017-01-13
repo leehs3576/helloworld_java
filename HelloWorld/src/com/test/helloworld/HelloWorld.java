c017b3e00d5d75c9235c53bcbe3843ef18ce77fb
이호성 <leehs3576@gmail.com>
2017-01-13 12:41:12 +0900

    initial code

---
+++ HelloWorld/src/com/test/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.test.helloworld;
+
+public class HelloWorld {
+	 public static void main(String[] args){
+		 System.out.println("Hello, welcome to the java world!");	
+	}
+}
+
+//프로그램 실행 진입점(메인함수) c++은 메인함수가 밖에 있으나 java는 클래스가 먼저
+//java는 클래스로 시작해서 클래스로 끝난다.
+//java에서 대문자 클래스, 상수
+//좋은 코드는 간결, 주석x
+//클래스를 생성할 때 대충대충
+
