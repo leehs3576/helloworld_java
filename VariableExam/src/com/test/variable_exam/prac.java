46adf13c21132bb7ad4add5e9db0a6c492d65185
이호성 <leehs3576@gmail.com>
2017-01-16 11:34:19 +0900

    initial

---
+++ VariableExam/src/com/test/variable_exam/prac.java
@@ -0,0 +1,41 @@
+package com.test.variable_exam;
+
+import java.util.Scanner;
+
+public class prac {
+	public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("숫자를 입력하세요.");
+	String inputString = scanner.nextLine();
+		
+	if(inputString.equals("1")){	
+		for(int i=1; i<6; i++){
+			for(int j=1; j<i+1; j++){
+				System.out.print("*");
+			}
+				System.out.println(" ");
+			}
+	}else if(inputString.equals("2")){
+		for(int i=1; i<6; i++){
+			for(int j=1; j<i+1; j++){
+				System.out.print("*");
+			}
+				System.out.println(" ");
+			}
+	}else if(inputString.equals("3")){
+		for(int i=1; i<6; i++){
+			for(int j=1; j<i+1; j++){
+				System.out.print("*");
+			}
+				System.out.println(" ");
+			}
+	}else if(inputString.equals("3")){
+		for(int i=1; i<6; i++){
+			for(int j=1; j<i+1; j++){
+				System.out.print("*");
+			}
+				System.out.println(" ");
+			}
+	}
+	}
+	}
